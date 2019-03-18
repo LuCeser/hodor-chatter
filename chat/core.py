f41d88bb09f793409dc5c8135931d25bc24f694a
zhanglu <fortunezlu@gmail.com>
2019-03-18 09:28:56 +0800

    feat: 客户信息写入本地

--- chat/core.py
+++ chat/core.py
@@ -1,9 +1,18 @@
 import itchat
+import pickle
+
+user_data_file = "user_data.pkl"
+
+
+def init_user_dict():
+    user_info = dict()
+    with open(user_data_file, 'wb') as f:
+        pickle.dump(user_info, f)
 
 
 # 调用接口或者方法获取返回值
 def get_response(msg):
-    userInfo = itchat.search_friends(userName=msg.fromUserName)
+    user = get_user_info(msg.fromUserName)
     reply = "请问您注册的邮箱是什么？"
     print("<<<< {0} : {1}".format(msg['User']['NickName'], reply))
     return reply
@@ -15,7 +24,35 @@ def get_user_info(user_id):
     :param user_id:
     :return:
     '''
-    pass
+    print("获取用户%s信息" % (user_id))
+    user_info = dict()
+    try:
+        with open(user_data_file, 'rb') as f:
+            user_info = pickle.load(f)
+            print(user_info)
+    except FileNotFoundError:
+        print("用户信息不存在")
+        init_user_dict()
+    except PermissionError:
+        print("无法读取用户权限信息")
+
+    if user_info.get(user_id):
+        print("用户已存在")
+    else:
+        print("当前客户 {0} 未注册".format(user_id))
+        add_user_info(user_id)
+
+    return ""
+
+
+def add_user_info(user_info):
+    '''
+    添加用户信息
+    :param user_id:
+    :return:
+    '''
+    with open(user_data_file, 'wb') as f:
+        f.write(user_info)
 
 
 @itchat.msg_register(itchat.content.TEXT)
@@ -23,8 +60,8 @@ def text_handler(msg):
     print(">>>> {0} : {1}".format(msg['User']['NickName'], msg['Text']))
 
     reply = get_response(msg)
-    defaultReply = "收到，稍后给您回复"
-    return reply or defaultReply
+    default_reply = "收到，稍后给您回复"
+    return reply or default_reply
 
 
 @itchat.msg_register(itchat.content.VOICE)
@@ -34,8 +71,8 @@ def voice_handler(msg):
 
 @itchat.msg_register(itchat.content.PICTURE)
 def picture_handler(msg):
-    defaultReply = "对不起，我现在还看不懂图片哦"
-    return defaultReply
+    default_reply = "对不起，我现在还看不懂图片哦"
+    return default_reply
 
 
 # 开启暂存登陆状态
