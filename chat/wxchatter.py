6bde0746a8da7d49a01cf06d196d5079f02daf78
zhanglu <fortunezlu@gmail.com>
2019-05-07 16:24:31 +0800

    feat: 增加build信息统一记录接口

--- chat/wxchatter.py
+++ chat/wxchatter.py
@@ -12,62 +12,83 @@ bot = Bot(cache_path=True, console_qr=True)
 principal_file = 'principal.pkl'
 
 
-@bot.register(Friend, TEXT)
-def friend_msg_handler(msg):
-    """
-    处理好友消息
-    :param msg:
-    :return:
-    """
-    print(">>>>> 收到 {} : {}".format(msg.sender.name, msg.text))
-
-
-# @bot.register(msg_types=FRIENDS)
-# def auto_accept_friends(msg):
+# @bot.register(Friend, TEXT)
+# def friend_msg_handler(msg):
 #     """
-#     自动接受好友请求
+#     处理好友消息
 #     :param msg:
 #     :return:
 #     """
-#     new_friends = msg.card.accept()
-#     new_friends.send('我们是同志了')
+#     print(">>>>> 收到 {} : {}".format(msg.sender.name, msg.text))
 
 
-class NotifyGroupApi(Resource):
+class BuildInfoApi(Resource):
+    """
+    记录构建结果
+    """
     def post(self):
+        """
+    {
+    "buildTime":"2019-04-12 19:22:30",
+    "serviceName":"roster-service",
+    "lastCommit":"9da3dk91bdk834q",
+    "commitLog":"feat: 增加新功能",
+    "imageName":"roster-service:191",
+    "buildResult":true,
+    "lastAuthor":"paul"
+    }
+    """
         req_json = request.get_json()
-        print(">>>> 向群聊: {} 发送构建结果通知".format(req_json['groupName']))
-        wx_group = bot.groups().search(req_json['groupName'])[0]
-        send_msg = '服务: {0} \r\n构建结果: {1}\r\n版本: {2}\r\n修改信息: {3}'\
-            .format(req_json['serviceName'], req_json['buildResult'], req_json['version'], req_json['commitMsg'])
-        wx_group.send(send_msg)
+        print(">>> 服务: {} 构建结果: {}".format(req_json['serviceName'], req_json['buildResult']))
+        if req_json['buildResult']:
+            self.notify_build_success(req_json)
+        else:
+            self.notify_build_failure(req_json)
 
-
-class NotifyFriendApi(Resource):
-    def post(self):
-        req_json = request.get_json()
-        print(">>>> 服务: {} 构建失败，向负责人发送通知通知".format(req_json['serviceName']))
+    def notify_build_success(self, build_info):
+        """
+        通知群组构建成功
+        :return:
+        """
         principal_dict = pickle.load(open(principal_file, 'rb'), encoding='utf-8')
-        principal = principal_dict[req_json['serviceName']]
+        group_name = principal_dict.get("groups").get(build_info['serviceName'])
+        print(">>> 通知群组 {} 服务 {} 构建成功".format(group_name, build_info['serviceName']))
+        send_msg = '服务: {0} \r\n构建时间: {1}\r\n镜像名: {2}\r\n提交信息: {3}'\
+            .format(build_info['serviceName'], build_info['buildTime'], build_info['imageName'], build_info['commitLog'])
+        self.notify_wx_group(group_name, send_msg)
 
-        wx_friend = bot.friends().search(principal)[0]
-        wx_friend.send(req_json['msg'])
+    def notify_build_failure(self, build_info):
+        """
+        通知服务负责人构建失败
+        :param build_info:
+        :return:
+        """
+        principal_dict = pickle.load(open(principal_file, 'rb'), encoding='utf-8')
+        friends_name = principal_dict.get("friends").get(build_info['serviceName'])
+        print(">>> 通知 服务 {} 负责人 {} 构建失败".format(build_info['serviceName'], friends_name))
+        send_msg = '服务: {0} \r\n构建时间: {1}\r\n镜像名: {2}\r\n提交信息: {3}' \
+            .format(build_info['serviceName'], build_info['buildTime'], build_info['imageName'], build_info['commitLog'])
+        self.notify_wx_friend(friends_name, send_msg)
+
+    @staticmethod
+    def notify_wx_group(group_name, msg):
+        print("<<< {}".format(msg))
+        wx_groups = bot.groups(update=True).search(group_name)
+        if wx_groups:
+            wx_groups[0].send(msg)
+        else:
+            print("找不到群聊 {}".format(group_name))
+
+    @staticmethod
+    def notify_wx_friend(friend, msg):
+        print("<<< {}".format(msg))
+        wx_friends = bot.friends().search(friend)
+        for friend in wx_friends:
+            friend.send(msg)
 
 
-api.add_resource(NotifyGroupApi, '/wx/notify/group', endpoint='group')
-api.add_resource(NotifyFriendApi, '/wx/notify/friend', endpoint='friend')
-
-# @app.route('/wx/alter/<string:group_name>', methods=['POST'])
-# def send_msg_to_group(group_name):
-#     if not request.json:
-#         print("没有请求信息")
-#         abort(400)
-#     return 'hi'
-#
-#
-# @app.errorhandler(404)
-# def not_found(error):
-#     return make_response(jsonify({'error': 'not found'}), 404)
+# 记录构建信息，并发送通知到服务负责人或测试人员
+api.add_resource(BuildInfoApi, '/wx/info', endpoint='cd')
 
 
 if __name__ == '__main__':
